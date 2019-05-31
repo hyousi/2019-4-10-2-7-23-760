cc1ab4351d4afd75a2fe3c62153050d9a0e25ddb
t0iiz <4649tz@gmail.com>
2019-05-31 19:26:54 +0800

    docs: finish hw04

--- main/main.js
+++ main/main.js
@@ -1,4 +1,11 @@
 module.exports = function main(inputs) {
     // write your code here...
+    if (inputs.distance<=2) {
+        return Math.round( 6 + 0.25*inputs.parkTime );
+    } else if (inputs.distance <=8) {
+        return Math.round( 6 + 0.8*(inputs.distance-2) +0.25*inputs.parkTime );
+    } else {
+        return Math.round(10.8 + 1.2*(inputs.distance-8) + 0.25*inputs.parkTime);
+    }
     return "price";
 };
